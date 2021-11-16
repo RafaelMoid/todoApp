a61f7e7751e8ce12f8321a7e138c00c943daf25f
Rafael Varela <rafaelvrcr@gmail.com>
2021-11-16 20:18:31 -0300

    Captura do valor do input (useState/onChange)

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from "./Button";
 import "./AddTask.css";
 
 
 const AddTask = () => {
+
+    //Criando state para salvar dados, Pode ser usado para as notas
+    const [inputData, setInputData] = useState('');
+
+    //Essa consta vai ser responsavel por setar o valor do inputData, usando o state e o onChange
+    const handleInputChange = (e) => {
+        setInputData(e.target.value);
+    }
+
+    //Na Div add-task-container podemos ver o uso do onChange
     return (
     <div className="add-task-container">
-        <input type="text" className="add-task-input"/>
+        <input 
+            onChange={handleInputChange}
+            className="add-task-input"
+            value={inputData}
+            type="text" 
+           
+        />
+        ;
         <div className="add-task-button-container">
             <Button>Adicionar</Button>
         </div>
